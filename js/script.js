288d5e06714a726ab146aed83d66de4d54719133
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-09-29 11:23:18 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+//Question 1
+
+for (var i = 0 ; i <= 7 ; i++ ) {
+    console.log(i); }
+
+//Question 2
+
+for (var i = 10; i >= 1 ; i = i - 1 ) {
+    console.log(i);
+}
+
+//question 3
+
+//My answer
+for (var i = 10; i < 20; i = i + 2) {
+    console.log(i+1)
+}
+
+//Proper answer
+for (var i = 10; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
